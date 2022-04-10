32455fb5cc9aed83b23a539f5fb951f681911475
P0zzle <leon.collazo.are@gmail.com>
2022-04-09 22:49:32 -0300

    commit on 'master'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
